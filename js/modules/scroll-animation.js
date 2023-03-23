77419fa615cfc2e58428afb8dff72411fb012b9c
Laion Gabriel <laiong.dev@gmail.com>
2023-03-23 07:19:51 -0300

    rearrange

--- js/modules/scroll-animation.js
+++ js/modules/scroll-animation.js
@@ -3,16 +3,23 @@ export default function initScrollAnimation() {
    const halfWindowHeight = window.innerHeight * 0.6;
 
    if (sections.length) {
-      function animateSections() {
-         sections.forEach((section) => {
-            const sectionTop = section.getBoundingClientRect().top;
-            const isSectionVisible = sectionTop - halfWindowHeight < 0;
-            if (isSectionVisible) section.classList.add("ativo");
-            // else if (section.classList.contains("ativo"))
-            //    section.classList.remove("ativo");
-         });
-         requestAnimationFrame(animateSections);
-      }
       animateSections();
+      window.addEventListener("scroll", () =>
+         requestAnimationFrame(animateSections)
+      );
+   }
+
+   function animateSections() {
+      const distance = [...sections].map((section) => {
+         const offset = section.offsetTop;
+         return {
+            element: section,
+            offset: Math.floor(offset) - halfWindowHeight,
+         };
+      });
+
+      distance.forEach((item) => {
+         if (window.scrollY > item.offset) item.element.classList.add("ativo");
+      });
    }
 }
