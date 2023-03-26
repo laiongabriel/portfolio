15d6ede30d135a03912747e71c88974b0e1ffde6
Laion Gabriel <laiong.dev@gmail.com>
2023-03-25 23:58:15 -0300

    skill hover timeout

--- js/modules/skill-hover.js
+++ js/modules/skill-hover.js
@@ -2,16 +2,20 @@ export default function initSkillHover() {
    const desc = document.querySelector(".skill-info");
    const initialText = desc.innerText;
    const skillList = document.querySelectorAll(".skill");
+   let timerId;
 
    if (window.innerWidth <= 800) desc.innerText = desc.dataset.mobile;
 
    skillList.forEach((skill) => {
       skill.addEventListener("mouseover", () => {
+         clearTimeout(timerId);
          desc.innerText = skill.dataset.description;
       });
       skill.addEventListener("mouseout", () => {
-         desc.innerText = initialText;
-         if (window.innerWidth <= 960) desc.innerText = desc.dataset.mobile;
+         timerId = setTimeout(() => {
+            desc.innerText = initialText;
+            if (window.innerWidth <= 960) desc.innerText = desc.dataset.mobile;
+         }, 300);
       });
    });
 }
